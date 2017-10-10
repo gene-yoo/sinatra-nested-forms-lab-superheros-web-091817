e64c5684623c8c3edecef25c8d1cd89a62016c99
gene-yoo <gene.y.yoo@gmail.com>
2017-10-10 17:50:24 -0400

    Lab complete.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, "app/views"
 
+  get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team = Team.new(params[:team])
+      @heros = params[:heros].collect do |hero_hash|
+        Superhero.new(hero_hash)
+      end
+
+      erb :team
+    end
+    
 end
